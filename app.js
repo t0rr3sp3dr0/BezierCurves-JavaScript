f6a940e89227e380a9d8e50f926329d62aff3f9e
Andre de Carli da Mota Silveira <acms@ug2c10.windows.cin.ufpe.br>
2017-06-06 13:49:23 -0300

    feat: display control points trajectory

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ class Point {
 }
 
 function deCasteljau(points, t) {
-    if (points.length == 1)
+    if (points.length === 1)
         return points[0];
     else {
         const newpoints = new Array(points.length - 1);
@@ -24,7 +24,7 @@ function deCasteljau(points, t) {
     }
 }
 
-function draw(context, curve, delta) {
+function draw(context, curve, delta, color) {
     let t = 0;
     const points = new Array((1 / delta) + 1);
     for (let i = 0; i < (1 / delta) + 1; i++) {
@@ -33,14 +33,14 @@ function draw(context, curve, delta) {
     }
 
     for (let i = 0; i < 1 / delta; i++)
-        drawLine(context, points[i], points[i + 1]);
+        drawLine(context, points[i], points[i + 1], color);
 }
 
-function drawLine(context, p1, p2) {
+function drawLine(context, p1, p2, color) {
     context.beginPath();
     context.moveTo(p1.x, p1.y);
     context.lineTo(p2.x, p2.y);
-    context.strokeStyle = 'white';
+    context.strokeStyle = color;
     context.stroke();
 }
 
@@ -54,6 +54,38 @@ function drawPolygonal(context, curve) {
     }
 }
 
+function drawPolygonals(context, curves) {
+    /* Caso precise colocar a função num botão
+        que desenha todas as poligonais
+     */
+    for (let i = 0; i < curves.length; i++) {
+        drawPolygonal(context, curves[i]);
+    }
+}
+
+function drawControlPoint(context, curve) {
+    for (let i = 0; i < curve.length; i++) {
+
+    }
+}
+
+function controlCurves(curves) {
+    /*  Dado um conjunto de curvas de bezier, calcular
+        novas curvas com os pontos de controle do conjunto.
+        Ex:
+        control[0] = { curves[0][0], curves[1][0], ... , curves[n][0] }
+        control[1] = { curves[0][1], curves[1][1], ... , curves[n][1] }
+     */
+    let control = [];
+    for (let i = 0; i < curves[0].length; i++) {
+        control.push([]);
+        for (let j = 0; j < curves.length - 1; j++) {
+            control[i].push(curves[j][i]);
+        }
+    }
+    return control;
+}
+
 const main = function () {
     const animation = parseFloat(prompt("Digite o tempo total de animação"));
     const interval = parseFloat(prompt("Digite o intervalo"));
@@ -61,30 +93,74 @@ const main = function () {
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
     const curves = [];
+    let dirty = false; // para saber se as curvas dos pontos de controle foram desenhadas
     curves.push([]);
 
     context.strokeStyle = 'white';
     context.lineWidth = 1;
     context.fillStyle = 'black';
-    context.fillRect(0, 0, 1024, 1024);
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
     console.log(degree);
     canvas.addEventListener('click', e => {
+        // Se tem curvas dos pontos de controle, pinte por cima
+        if (curves.length > 2 && dirty === true) {
+            let control = controlCurves(curves);
+            for (let i = 0; i < control.length; i++) {
+                draw(context, control[i], interval/animation, 'black');
+            }
+        }
         console.log(new Point(e.pageX, e.pageY));
-        console.log(curves);
         if (curves[curves.length - 1].length < degree){
             curves[curves.length - 1].push(new Point(e.pageX, e.pageY));
+            // Desenha os pontos de controle
             context.beginPath();
             context.arc(e.pageX, e.pageY, 5, 0, 2*Math.PI);
             context.strokeStyle = 'red';
             context.stroke();
         }
-        if (curves[curves.length - 1].length == degree) {
-            draw(context, curves[curves.length - 1], interval / animation);
+        console.log(curves);
+        if (curves[curves.length - 1].length === degree) {
+            draw(context, curves[curves.length - 1], interval / animation, 'white');
             drawPolygonal(context, curves[curves.length - 1]);
             curves.push([]);
+            if (dirty)
+                dirty = false;
+
         }
     });
+    document.getElementById("btnShowControlCurves").onclick = function() {
+        /*  Se tem curvas suficientes para desenhar curvas dos pontos de controle, desenhe
+            Isso deverá ser apagado no futuro, pra que não atrapalhe a visualização
+        */
+        if (curves.length > 2) {
+            dirty = true;
+            let control = controlCurves(curves);
+            console.log(control);
+            for(let i = 0; i < control.length; i++) {
+                draw(context, control[i], interval/animation, 'blue');
+            }
+
+        }
+    }
+    // ==>A fazer<==
+    // document.getElementById("btnPlay").onclick = function() {
+    //     if (curves.length > 2) {
+    //         let control = controlCurves(curves);
+    //         for (let t = 0; t < 1; t += interval/animation) {
+    //             let points = [];
+    //             for (let i = 0; i < control.length; i++) {
+    //                 points.push(deCasteljau(control[i], t));
+    //             }
+    //
+    //         }
+    //
+    //         let tempCurve = [];
+    //
+    //     }
+    // }
 };
+
+
 
 main();
